d1e31499b41de7f69ff5f99dccecb128527bd9a4
Sinjin <sinjinmikal10@gmail.com>
2017-11-20 17:48:47 -0800

    Fixed profile picture

--- app/src/main/java/com/example/jonnel/parkhere/EditAccountActivity.java
+++ app/src/main/java/com/example/jonnel/parkhere/EditAccountActivity.java
@@ -2,9 +2,17 @@ package com.example.jonnel.parkhere;
 
 import android.app.Activity;
 import android.content.Context;
+import android.content.CursorLoader;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.database.Cursor;
 import android.net.Uri;
+import android.os.Build;
+import android.provider.DocumentsContract;
+import android.provider.MediaStore;
 import android.support.annotation.NonNull;
+import android.support.annotation.RequiresApi;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.TextUtils;
@@ -15,13 +23,19 @@ import android.widget.EditText;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.auth.UserProfileChangeRequest;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
 
+import java.io.File;
 import java.net.URI;
 
 public class EditAccountActivity extends AppCompatActivity {
@@ -30,7 +44,8 @@ public class EditAccountActivity extends AppCompatActivity {
     private EditText changeEmail;
     private EditText changePassword;
     private String uid= FirebaseAuth.getInstance().getCurrentUser().getUid();
-
+    FirebaseStorage storage= FirebaseStorage.getInstance();
+    StorageReference storageRef = storage.getReference();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -46,12 +61,37 @@ public class EditAccountActivity extends AppCompatActivity {
                 updateAccount();
             }
         });
+
+
+
         choosePicture.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                chooseGooglePicture();
+                ActivityCompat.requestPermissions(EditAccountActivity.this, new String[]{android.Manifest.permission.READ_EXTERNAL_STORAGE},1);
+
             }
         });
+
+    }
+    public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case 1: {
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    // permission granted and now can proceed
+                    chooseGooglePicture(); //a sample method called
+
+                } else {
+
+                    // permission denied, boo! Disable the
+                    // functionality that depends on this permission.
+                    Toast.makeText(EditAccountActivity.this, "Permission denied to read your External storage", Toast.LENGTH_SHORT).show();
+                }
+                return;
+            }
+            // add other cases for more permissions
+        }
     }
 
     private void chooseGooglePicture(){
@@ -66,15 +106,31 @@ public class EditAccountActivity extends AppCompatActivity {
     public void onActivityResult(int requestCode, int resultCode, Intent data)
     {
         FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
-        Intent profile= new Intent(this,ProfileActivity.class);
+        Intent profile= new Intent(this,UserActivity.class);
         if (requestCode == PICK_IMAGE&&resultCode== Activity.RESULT_OK) {
-            Uri selected= data.getData();
-            DatabaseReference dref = FirebaseDatabase.getInstance().getReference();
-            dref=dref.child("User Id: " + uid).child("User Information");
-            dref.child("photoURI").setValue(selected.toString());
+            Uri selected=data.getData();
+            String path = getPath(EditAccountActivity.this,selected);
+            StorageReference childReference = storageRef.child(uid);
+            Uri file = Uri.fromFile(new File(path));
+            UploadTask uploadTask= childReference.putFile(file);
+            uploadTask.addOnFailureListener(new OnFailureListener() {
+                @Override
+                public void onFailure(@NonNull Exception exception) {
+                    // Handle unsuccessful uploads
+                }
+            }).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                @Override
+                public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                    // taskSnapshot.getMetadata() contains file metadata such as size, content-type, and download URL.
+                    Uri downloadUrl = taskSnapshot.getDownloadUrl();
+                }
+            });
+            //DatabaseReference dref = FirebaseDatabase.getInstance().getReference();
+            //dref=dref.child("User Id: " + uid).child("User Information");
+            //dref.child("photoURI").setValue(selected.toString());
         }
         startActivity(profile);
-        finish();
+
     }
 
     private void updateAccount()
@@ -129,5 +185,30 @@ public class EditAccountActivity extends AppCompatActivity {
     }
 
 
+    public String getPath(Context context, Uri uri ) {
+        String filePath = "";
+        String wholeID = DocumentsContract.getDocumentId(uri);
 
+        // Split at colon, use second item in the array
+        String id = wholeID.split(":")[1];
+
+        String[] column = { MediaStore.Images.Media.DATA };
+
+        // where id is equal to
+        String sel = MediaStore.Images.Media._ID + "=?";
+
+        Cursor cursor = context.getContentResolver().query(MediaStore.Images.Media.EXTERNAL_CONTENT_URI,
+                column, sel, new String[]{ id }, null);
+
+        int columnIndex = cursor.getColumnIndex(column[0]);
+
+        if (cursor.moveToFirst()) {
+            filePath = cursor.getString(columnIndex);
+        }
+        cursor.close();
+        return filePath;
+    }
 }
+
+
+
